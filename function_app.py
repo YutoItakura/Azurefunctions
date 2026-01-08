8074d6d48e34d50ef9b5a8bbbe646be1e99a6c27
YutoItakura <y1120hafadai@outlook.jp>
2026-01-08 11:42:42 +0900

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -12,22 +12,29 @@ app = func.FunctionApp()
 
 @app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+
     encoded_password = urllib.parse.quote(password)
-    connection_string = f"mssql+pymssql://{username}:{encoded_password}@{server}/{database}"
+    connection_string = (f"mssql+pymssql://{username}:{encoded_password}@{server}/{database}"
+                         f"?charset=utf8")
 
     today = datetime.date.today()
+    olddatetime=today - datetime.timedelta(days=7)
     today_str = today.strftime('%Y-%m-%d')
+    olddatetime_str = olddatetime.strftime('%Y-%m-%d')
 
     engine = create_engine(connection_string)
+    data=[]
 
     try:
         with engine.connect() as conn:
-            result = conn.execute(text("SELECT * FROM DISEASES WHERE Date = :date"), {'date': today_str})
+            result = conn.execute(text("SELECT * FROM DISEASES WHERE Date = :date1 OR Date = :date2"), {'date1': today_str,'date2': olddatetime_str})
             for row in result:
+                date= row._mapping['Date']
+                cameraid= row._mapping['CameraID']
                 disease1= row._mapping['DISEASE1']
                 disease2= row._mapping['DISEASE2']
                 disease3= row._mapping['DISEASE3']
-                print(f"Date: {today_str}, Disease1: {disease1}, Disease2: {disease2}, Disease3: {disease3}")
+                print(f"Date: {date}, CameraID: {cameraid} Disease1: {disease1}, Disease2: {disease2}, Disease3: {disease3}")
                 numdisease=sum([disease1, disease2, disease3])
                 if numdisease == 0:
                     diseaselevel=0
@@ -41,20 +48,17 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
                     diseaselevel=0.8
                 elif numdisease > 12:
                     diseaselevel=1.0
-        #make json response       
+                data.append({
+                    "Date":str(date),
+                    "CameraID": cameraid,
+                    "Disease1": disease1,
+                    "Disease2": disease2,
+                    "Disease3": disease3,
+                    "DiseaseLevel": diseaselevel
+                })
+         
         response_data = {
-            "tree1":diseaselevel,
-            "leaves1":diseaselevel,
-            "tree2":0,
-            "leaves2":0,
-            "tree3":0,
-            "leaves3":0,
-            "tree4":0,
-            "leaves4":0,
-            "tree5":0,
-            "leaves5":0,
-            "tree6":0,
-            "leaves6":0,
+            "data": data
             }
         return func.HttpResponse(json.dumps(response_data), mimetype="application/json")
     
